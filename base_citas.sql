29d456c3019d213c3dc371344ab1754c0e4762d6
PedroMatadamas <32680379+PedroMatadamas@users.noreply.github.com>
2020-04-02 22:58:17 -0600

    Update base_citas.sql

--- base_citas.sql
+++ base_citas.sql
@@ -1,5 +1,4 @@
 create database if not exists agenda_db;
-
 use agenda_db;
 
 create table if not exists contacto(
@@ -12,7 +11,7 @@ create table if not exists contacto(
     primary key(id_contacto)
 )engine = InnoDB;
 
-create table if not exists cps( #Codigos postales
+create table if not exists cps(
 	cp int not null,
     ciudad varchar(45) not null,
 	estado varchar(45) not null,
@@ -27,10 +26,11 @@ create table if not exists lugar(
 	calle varchar(45) NOT NULL,
     l_cp int,
     primary key(id_lugar),
-	constraint fkcp_lug foreign key(l_cp)
-		references cps(cp)
-        on delete set null
-        on update cascade
+	constraint fkcp_lug 
+    foreign key(l_cp)
+	references cps(cp)
+	on delete set null
+	on update cascade
 )ENGINE = InnoDB;
 
 create table if not exists cita(
@@ -40,21 +40,23 @@ create table if not exists cita(
 	asunto varchar(45) not null,
     c_id_lugar int,
     primary key(id_cita),
-	constraint fkc_lug foreign key(c_id_lugar)
-		references lugar(id_lugar)
-        on delete set null
-        on update cascade
+	constraint fkc_lug 
+    foreign key(c_id_lugar)
+	references lugar(id_lugar)
+	on delete set null
+	on update cascade
 )engine = InnoDB;
 
 create table if not exists citas(
 	id_contacto_citas int not null,
     id_cita_citas int not null,
     primary key(id_contacto_citas, id_cita_citas),
-    
-    constraint fk_contacto_citas foreign key(id_contacto_citas)
-		references contacto(id_contacto)
-        on update cascade,
-	constraint fk_cita_citas foreign key(id_cita_citas)
-		references cita(id_cita)
-        on update cascade
+    constraint fk_contacto_citas 
+    foreign key(id_contacto_citas)
+	references contacto(id_contacto)
+	on update cascade,
+	constraint fk_cita_citas 
+    foreign key(id_cita_citas)
+	references cita(id_cita)
+	on update cascade
 )engine = InnoDB;
